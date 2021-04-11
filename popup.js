333a5d5009637359d658e17f37c3cf51727f5cc1
Jijo James <jijojames18@gmail.com>
2021-04-11 12:28:22 +0530

    Set correct status in popup on load

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@
 
 const PORT_NAME = "who_is_reviewing";
 
-const EVENT_TYPE_POST = "POST";
+const EVENT_TYPE_STATUS_CHANGE = "STATUS_CHANGE";
 
 const extensionWindow = (function () {
   if (typeof chrome !== undefined) {
@@ -17,5 +17,19 @@ const extensionWindow = (function () {
 const checkbox = document.getElementById("toggle-status");
 
 checkbox.addEventListener("change", (event) => {
-  console.log(event.currentTarget.checked);
+  const query = { active: true, currentWindow: true };
+  const prStatus = event.currentTarget.checked ? "STARTED" : "STOPPED";
+  function callback(tabs) {
+    extensionWindow.tabs.sendMessage(tabs[0].id, {
+      status: prStatus,
+      eventType: EVENT_TYPE_STATUS_CHANGE,
+    });
+  }
+  chrome.tabs.query(query, callback);
+});
+
+extensionWindow.runtime.onMessage.addListener((event) => {
+  if (event.eventType === EVENT_TYPE_STATUS_CHANGE) {
+    checkbox.checked = event.status;
+  }
 });
