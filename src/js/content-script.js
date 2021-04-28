eba3ccd725e16a1cd1c73659a961fc8ef67f6cd4
Jijo James <jijojames18@gmail.com>
2021-04-28 12:49:44 +0530

    Refactor content script

--- src/js/content-script.js
+++ src/js/content-script.js
@@ -10,76 +10,84 @@ import {
 import extensionWindow from "@js/common/context";
 import { getPRPath, getUserLogin } from "@js/common/functions";
 
-const CONTAINER_TO_APPEND_TO = ".js-merge-message-container";
-const CONTAINER_ELEM_CLASS = "who-is-reviewing-user-list";
-
 const path = getPRPath(document.URL);
 
-let userLogin = "";
-
-const drawReviewerList = (userList) => {
-  // Remove any existing elements
-  const existingUserListElem = document.querySelectorAll(
-    `.${CONTAINER_ELEM_CLASS}`
-  );
-  for (let i = 0; i < existingUserListElem.length; i++) {
-    existingUserListElem[i].remove();
+class ContentScript {
+  constructor(userLogin, path, config) {
+    this.userLogin = userLogin;
+    this.path = path;
+    this.config = config;
   }
 
-  let userListText = "";
-  for (let i = 0; i < userList.length; i++) {
-    userListText += ` ${userList[i]} `;
-  }
-  const userListTextMessage = config.msg.userList.replace(
-    "{USER_LIST}",
-    userListText
-  );
+  drawReviewerList() {
+    const CONTAINER_TO_APPEND_TO = ".js-merge-message-container";
+    const CONTAINER_ELEM_CLASS = "who-is-reviewing-user-list";
 
-  const containerElem = document.querySelectorAll(CONTAINER_TO_APPEND_TO);
-  if (containerElem.length === 1) {
-    const userListContainerElem = document.createElement("div");
-    userListContainerElem.classList.add(CONTAINER_ELEM_CLASS);
-    userListContainerElem.prepend(userListTextMessage);
-    // Append the element to DOM.
-    containerElem[0].prepend(userListContainerElem);
-  }
-};
-
-const callServiceWorker = (params) => {
-  var port = extensionWindow.runtime.connect({ name: PORT_NAME });
-  port.postMessage({ path, ...params });
-  port.onMessage.addListener(function (msg) {
-    let amIReviewing = false;
-    if (msg.status === STATUS_OK) {
-      const { userList } = msg;
-      drawReviewerList(userList);
-      amIReviewing = userList.indexOf(userLogin) >= 0;
-    } else {
-      drawReviewerList([]);
+    // Remove any existing elements
+    const existingUserListElem = document.querySelectorAll(
+      `.${CONTAINER_ELEM_CLASS}`
+    );
+    for (let i = 0; i < existingUserListElem.length; i++) {
+      existingUserListElem[i].remove();
     }
-    extensionWindow.storage.sync.set({
-      [path]: {
-        status: "OPEN",
-        amIReviewing,
-        timestamp: new Date().getTime(),
-      },
-    });
-  });
-};
+
+    let userListText = "";
+    for (let i = 0; i < this.userList.length; i++) {
+      userListText += ` ${this.userList[i]} `;
+    }
+    const userListTextMessage = this.config.msg.userList.replace(
+      "{USER_LIST}",
+      userListText
+    );
+
+    const containerElem = document.querySelectorAll(CONTAINER_TO_APPEND_TO);
+    if (containerElem.length === 1) {
+      const userListContainerElem = document.createElement("div");
+      userListContainerElem.classList.add(CONTAINER_ELEM_CLASS);
+      userListContainerElem.prepend(userListTextMessage);
+      // Append the element to DOM.
+      containerElem[0].prepend(userListContainerElem);
+    }
+  }
+
+  callServiceWorker(params) {
+    var port = extensionWindow.runtime.connect({ name: PORT_NAME });
+    port.postMessage({ path: this.path, ...params });
+    port.onMessage.addListener(
+      function (msg) {
+        this.amIReviewing = false;
+        if (msg.status === STATUS_OK) {
+          this.userList = msg.userList;
+          this.amIReviewing = this.userList.indexOf(this.userLogin) >= 0;
+          this.drawReviewerList();
+        } else {
+          this.drawReviewerList([]);
+        }
+        extensionWindow.storage.sync.set({
+          [path]: {
+            status: "OPEN",
+            amIReviewing: this.amIReviewing,
+            timestamp: new Date().getTime(),
+          },
+        });
+      }.bind(this)
+    );
+  }
+}
 
 // Execute logic only for PR pages.
 if (path) {
   // Get login of user
-  userLogin = getUserLogin(document.getElementsByTagName("meta"));
-
-  callServiceWorker({
+  const userLogin = getUserLogin(document.getElementsByTagName("meta"));
+  const contentScript = new ContentScript(userLogin, path, config);
+  contentScript.callServiceWorker({
     eventType: EVENT_TYPE_GET,
   });
 
   extensionWindow.runtime.onMessage.addListener(async (event) => {
     // Update list when current user toggles status in Popup
     if (event.eventType === EVENT_TYPE_STATUS_CHANGE) {
-      callServiceWorker({
+      contentScript.callServiceWorker({
         eventType: EVENT_TYPE_POST,
         userId: userLogin,
         status: event.status,
